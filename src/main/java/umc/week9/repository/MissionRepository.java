d4439c6327dae842ad0949b3b131659a5f645bdd
ryogaeng <fbgyrud828@naver.com>
2024-06-26 20:42:13 +0900

    [FEAT] 특정 가게의 미션 목록 조회 API

--- src/main/java/umc/week9/repository/MissionRepository.java
+++ src/main/java/umc/week9/repository/MissionRepository.java
@@ -1,7 +1,12 @@
 package umc.week9.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.week9.domain.Mission;
+import umc.week9.domain.Store;
 
 public interface MissionRepository extends JpaRepository<Mission, Long> {
+    Page<Mission> findAllByStore(Store store, PageRequest pageRequest);
+
 }
--- src/main/java/umc/week9/service/MemberService/MissionService.java
+++ src/main/java/umc/week9/service/MemberService/MissionService.java
@@ -1,8 +1,12 @@
 package umc.week9.service.MemberService;
 
+import org.springframework.data.domain.Page;
+import umc.week9.domain.Mission;
 import umc.week9.web.dto.MissionRequestDTO;
 import umc.week9.web.dto.MissionResponseDTO;
 
 public interface MissionService {
     MissionResponseDTO addMission(MissionRequestDTO requestDTO);
+    Page<Mission> getMissionList(Long storeId, int page);
+
 }
